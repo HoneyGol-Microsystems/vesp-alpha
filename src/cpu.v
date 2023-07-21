e74c863d8d9768e56a8186a132eaac4a8651f0bb
Jan Medek <medekja5@fit.cvut.cz>
2023-07-21 12:44:09 +0200

    formatting, convert to camelCase

--- src/cpu.v
+++ src/cpu.v
@@ -2,12 +2,12 @@ module cpu (
     input         clk,
     input         reset,
     input  [31:0] instruction,
-    input  [31:0] data_from_mem,
-    output        WE, // write enable to data memory
-    output        ex, // exception TODO: type of exception
+    input  [31:0] dataFromMem,
+    output        WE,     // write enable to data memory
+    output        except, // exception TODO: type of exception
     output [31:0] PC,
-    output [31:0] mem_addr,
-    output [31:0] data_to_mem
+    output [31:0] memAddr,
+    output [31:0] dataToMem
 );
 
 endmodule
