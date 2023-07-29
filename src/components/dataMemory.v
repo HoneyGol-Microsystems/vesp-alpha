01989c4b5e74d61d27de6913317262ab4a3baa3a
Jan Medek <medekja5@fit.cvut.cz>
2023-07-29 11:40:55 +0200

    write according to mask

--- src/components/dataMemory.v
+++ src/components/dataMemory.v
@@ -2,6 +2,7 @@ module dataMemory (
     input         clk,
     input         reset,
     input         we,
+    input  [3:0]  mask,
     input  [31:0] a,
     input  [31:0] di,
     output [31:0] do
@@ -17,7 +18,21 @@ module dataMemory (
 
     always @(posedge clk) begin
         if (we) begin
-            ram[a[31:2]] <= di;
+            if (mask[0]) begin
+                ram[a[31:2]][7:0] <= di[7:0];
+            end
+
+            if (mask[1]) begin
+                ram[a[31:2]][15:8] <= di[15:8];
+            end
+
+            if (mask[2]) begin
+                ram[a[31:2]][23:16] <= di[23:16];
+            end
+
+            if (mask[3]) begin
+                ram[a[31:2]][31:24] <= di[31:24];
+            end
         end
     end
 
