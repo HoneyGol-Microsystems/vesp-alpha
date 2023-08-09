c278623b91057d64806b19e4bbcee5966deae100
Jan Medek <medekja5@fit.cvut.cz>
2023-08-09 22:08:38 +0200

    implement topTest and error detection

--- tests/rvtests/topTest.v
+++ tests/rvtests/topTest.v
@@ -1,10 +1,14 @@
 `include "src/components/top.v"
+`include "src/constants.vh"
 
 `define __MKPY_CURRENT_TEST "PATH_TO_HEX"
+`define ECALL               32'b1110011
+`define EBREAK              32'b100000000000001110011
 
 module topTest();
     
     reg clk, reset;
+    integer i;
 
     top dut(
         .sysClk(clk),
@@ -20,7 +24,9 @@ module topTest();
         reset <= 1;
         #1;
         reset <= 0;
+        #99999;
 
+        $display("TIMEOUT");
         $finish;
     end
 
@@ -28,5 +34,22 @@ module topTest();
 		clk <= 1; #1;
         clk <= 0; #1;
 	end
+
+    always @(posedge clk) begin
+        if (dut.instrBusData == `ECALL) begin
+            $display(`ASSERT_SUCCESS);
+            $finish;
+        end
+
+        if (dut.instrBusData == `EBREAK) begin
+            $display(`ASSERT_FAIL);
+            $finish;
+        end
+
+        // if (dut.cpu.PC == 'h88) begin
+        //     $display("DEBUG_STOP");
+        //     $finish;
+        // end
+    end
     
 endmodule
