44ec1fa0eca569946e77e763284c023a5a17796e
Jan Medek <medekja5@fit.cvut.cz>
2023-08-26 22:46:19 +0200

    redo alu for CSR support

--- src/components/alu.v
+++ src/components/alu.v
@@ -16,17 +16,18 @@ module alu #(
     // decode operation
     always @(*) begin
         case (ctrl)
-            4'b0000: res = $signed(op1) + $signed(op2);
-            4'b0001: res = $signed(op1) - $signed(op2);
-            4'b0010: res = op1 & op2;
-            4'b0011: res = op1 | op2;
-            4'b0100: res = op1 ^ op2;
-            4'b0101: res = op1 << op2[4:0]; // shift amount is encoded in the lower 5 bits
-            4'b0110: res = op1 >> op2[4:0];
-            4'b0111: res = $signed(op1) >>> op2[4:0];
-            4'b1000: res = $signed(op1) < $signed(op2);
-            4'b1001: res = op1 < op2;
-            default: res = 0;
+            4'b0000: res = op1;
+            4'b0001: res = $signed(op1) + $signed(op2);
+            4'b0010: res = $signed(op1) - $signed(op2);
+            4'b0011: res = op1 & op2;
+            4'b0100: res = op1 & ~op2;
+            4'b0101: res = op1 | op2;
+            4'b0110: res = op1 ^ op2;
+            4'b0111: res = op1 << op2[4:0]; // shift amount is encoded in the lower 5 bits
+            4'b1000: res = op1 >> op2[4:0];
+            4'b1001: res = $signed(op1) >>> op2[4:0];
+            4'b1010: res = $signed(op1) < $signed(op2);
+            default: res = op1 < op2;
         endcase
     end
 
