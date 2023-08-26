21229f48abe1276b6dd54d480779906a636a5e66
Jan Medek <medekja5@fit.cvut.cz>
2023-08-26 15:13:30 +0200

    add comments

--- scripts/elftohex.sh
+++ scripts/elftohex.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+# check if all necessary arguments were passed
 if [[ $# -ne 2 ]]; then
-    echo "usage: $0 PATH_TO_ELF_FILES DEST_FOR_HEX_FILES"
+    echo "usage: $0 ABS_PATH_TO_ELF_FILES ABS_DEST_FOR_HEX_FILES"
     exit 1
 fi
 
 elfSrcPath=$1
 hexDestPath=$2
-elfFiles=$(scanelf -E ET_EXEC -BF %F "$elfSrcPath")
+elfFiles=$(scanelf -E ET_EXEC -BF %F "$elfSrcPath") # get all elf files with ET_EXEC format
 
 for f in $elfFiles; do
-    fName=$(basename "$f")
-    elf2hex --bit-width 32 --input "$f" --output "$hexDestPath/$fName.hex"
+    fNoExt=$(basename "$f") # get file name without extension
+    elf2hex --bit-width 32 --input "$f" --output "$hexDestPath/$fNoExt.hex" # convert elf file to .hex format
 done
