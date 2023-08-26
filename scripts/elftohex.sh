23297794e50bfdadb3ce32ca095b4e3f0461da5a
Jan Medek <medekja5@fit.cvut.cz>
2023-08-26 20:35:25 +0200

    args don't have to be abs paths

--- scripts/elftohex.sh
+++ scripts/elftohex.sh
@@ -2,7 +2,7 @@
 
 # check if all necessary arguments were passed
 if [[ $# -ne 2 ]]; then
-    echo "usage: $0 ABS_PATH_TO_ELF_FILES ABS_DEST_FOR_HEX_FILES"
+    echo "usage: $0 PATH_TO_ELF_FILES DEST_FOR_HEX_FILES"
     exit 1
 fi
 
@@ -11,6 +11,6 @@ hexDestPath=$2
 elfFiles=$(scanelf -E ET_EXEC -BF %F "$elfSrcPath") # get all elf files with ET_EXEC format
 
 for f in $elfFiles; do
-    fNoExt=$(basename "$f") # get file name without extension
+    fNoExt=$(basename "$f") # get file name without path
     elf2hex --bit-width 32 --input "$f" --output "$hexDestPath/$fNoExt.hex" # convert elf file to .hex format
 done
