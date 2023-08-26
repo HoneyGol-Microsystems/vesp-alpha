7f5a644c35e9f03a60665646fed2201bfc1dd5b2
Jan Medek <medekja5@fit.cvut.cz>
2023-08-26 20:34:56 +0200

    don't change directories

--- scripts/asmtohex.sh
+++ scripts/asmtohex.sh
@@ -2,24 +2,20 @@
 
 # check if all necessary arguments were passed
 if [[ $# -ne 2 ]]; then
-    echo "usage: $0 ABS_PATH_TO_ASM_FILES ABS_DEST_FOR_HEX_FILES"
+    echo "usage: $0 PATH_TO_ASM_FILES DEST_FOR_HEX_FILES"
     exit 1
 fi
 
-oldPath=$PWD
 asmSrcPath=$1
 hexDestPath=$2
-cd $asmSrcPath
-asmFiles=$(ls *.S)
+asmFiles=$(ls $asmSrcPath*.S)
 scriptPath=$(dirname $0) # location of this script
 
 for f in $asmFiles; do
-   fNoExt=$(basename "$f" .S) # get file name without extension
+   fNoExt=$(basename "$f" .S) # get file name without path and it's extension
    riscv64-unknown-elf-gcc -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -march=rv32i -mabi=ilp32 "$f" -o "$fNoExt" # compile source files
 
     mkdir -p "$hexDestPath"
     "$scriptPath/elftohex.sh" . "$hexDestPath" # run script to convert elf file to .hex format
     rm $fNoExt # remove elf file
 done
-
-cd $oldPath
