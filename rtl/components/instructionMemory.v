65dd444951c0f7d0f2e7a68502e8e765bc1ad219
Jan Medek <medekja5@fit.cvut.cz>
2023-10-21 17:38:54 +0200

    update memory writing

--- rtl/components/instructionMemory.v
+++ rtl/components/instructionMemory.v
@@ -2,13 +2,20 @@
 `define __FILE_INSTRUCTION_MEMORY_V
 
 module instructionMemory #(
-    parameter WORD_CNT = 16 // number of words (32b) in memory
+    parameter WORD_CNT = 16, // number of words (32b) in memory
+    parameter MEM_FILE = ""
 ) (
     input  [31:0] a,
     output [31:0] d
 );
 
     reg [31:0] ram [WORD_CNT-1:0];
+
+    initial begin
+        if (MEM_FILE != "") begin
+            $readmemh(MEM_FILE, ram, 0, WORD_CNT-1);
+        end
+    end
 
     assign d = ram[a[31:2]];
 
