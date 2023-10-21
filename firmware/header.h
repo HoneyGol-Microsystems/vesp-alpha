540bc1b7100cfb1ad4ec148d1e89afa8abd4c3b6
Jan Medek <medekja5@fit.cvut.cz>
2023-10-21 16:13:01 +0200

    rename asm/ to firmware/, add startup code things

---
+++ firmware/header.h
@@ -0,0 +1,22 @@
+#ifndef __HEADER_H
+#define __HEADER_H
+
+#include <stdint.h>
+
+/* GPIO addresses */
+volatile uint8_t * const GPIOWR_A_PTR  = (volatile uint8_t *) 0xF0000000;
+volatile uint8_t * const GPIODIR_A_PTR = (volatile uint8_t *) 0xF0000001;
+volatile uint8_t * const GPIORD_A_PTR  = (volatile uint8_t *) 0xF0000002;
+volatile uint8_t * const GPIOWR_B_PTR  = (volatile uint8_t *) 0xF0000003;
+volatile uint8_t * const GPIODIR_B_PTR = (volatile uint8_t *) 0xF0000004;
+volatile uint8_t * const GPIORD_B_PTR  = (volatile uint8_t *) 0xF0000005;
+
+/* GPIO address placeholders */
+#define GPIOWR_A  *GPIOWR_A_PTR
+#define GPIODIR_A *GPIODIR_A_PTR
+#define GPIORD_A  *GPIORD_A_PTR
+#define GPIOWR_B  *GPIOWR_B_PTR
+#define GPIODIR_B *GPIODIR_B_PTR
+#define GPIORD_B  *GPIORD_B_PTR
+
+#endif // __HEADER_H
