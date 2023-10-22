9cc38af088d0079de5828976738218e4d38259dd
Ondrej Golasowski <golasowski.o@gmail.com>
2023-10-22 20:22:33 +0200

    Finished basic recipe support

--- scripts/recipeProcessor.py
+++ scripts/recipeProcessor.py
@@ -21,6 +21,7 @@ class RecipeProcessor:
 
     def __init__(self, recipePath):
         _LOGGER.debug(f"Loading recipe: {recipePath}")
+        self.fileName = str(recipePath)
         with recipePath.open() as file:
             self.recipe = yaml.safe_load(file)
 
@@ -46,12 +47,18 @@ class RecipeProcessor:
 
     def stepRun(self, stepData) -> bool:
 
+        _LOGGER.info("Starting run step...")
+
         try:
             execName             = stepData["executable"]
-            assertConds          = stepData["assert"]
             globalSource : Path  = stepData["global_source"]
-            
-            params        = []
+
+        except KeyError:
+            _LOGGER.error("Missing required key found during elaboration of run step. Halting.")
+            return False
+        
+        params = []
+        try:
             # Check if there is a source placeholder in params and replace accordingly.
             for par in stepData["params"]:
                 if par == self.PLACEHOLDER_CURRENT_SOURCE:
@@ -61,23 +68,61 @@ class RecipeProcessor:
                         params.append(str(globalSource.resolve()))
                 else:
                     params.append(par)
-
         except KeyError:
-            _LOGGER.error("Missing required key found during elaboration of run step. Halting.")
-            return False
+            pass
+
+        try:
+            assertReturnCode = stepData["assert"]["return_code"] 
+        except KeyError:
+            assertReturnCode = None
+
+        try:
+            assertOutputNotCont = stepData["assert"]["output"]["not_contains"]
+        except KeyError:
+            assertOutputNotCont = []
+        
+        try:
+            assertOutputCont    = stepData["assert"]["output"]["contains"]
+        except KeyError:
+            assertOutputCont    = []
 
         _LOGGER.debug(f"Elaborated params: {params}")
         
-        _LOGGER.debug(f"Running executable '{execName}'")
-        execWithParams = ["echo", *params]
+        execWithParams = [execName, *params]
+        _LOGGER.debug(f"Running: '{execWithParams}'")
 
-        ret = subprocess.run(
+        pipes = subprocess.Popen(
             execWithParams,
             stdout = subprocess.PIPE,
             stderr = subprocess.PIPE
         )
+
+        stdout, stderr = pipes.communicate()
+        stdout = stdout.decode('utf-8')
+        stderr = stderr.decode('utf-8')
+
+        if stdout: _LOGGER.info(f"STDOUT: {stdout}")
+        if stderr: _LOGGER.info(f"STDERR: {stderr}")
+
+        if assertReturnCode:
+            if pipes.returncode != assertReturnCode:
+                _LOGGER.info(f"Assertion for return code failed. Expected {assertReturnCode} got {pipes.returncode}")
+                return False
+            
+        if type(assertOutputCont) == list:
+            for outCont in assertOutputCont:
+                if not outCont in (stdout + stderr):
+                    _LOGGER.info(f"Assertion failed for program output condition: '{outCont}' not found in output.")
+                    return False
         
-        return False
+        if type(assertOutputNotCont) == list:
+            for outCont in assertOutputNotCont:
+                if outCont in (stdout + stderr):
+                    _LOGGER.info(f"Assertion failed for program output condition: '{outCont}' was found in output.")
+                    return False
+        
+        _LOGGER.info("Step passed.")
+        return True
 
     stepParsers : dict = {
         "make_directory" : stepMakeDirectory,
@@ -154,21 +199,21 @@ class RecipeProcessor:
             self.sources = [ None ]
             _LOGGER.debug(f"No sources specified, inserted dummy one: {self.sources}")
         
-        print(f"Running recipe {self.name}...")
+        print(f"📜 Running recipe '{self.name}' ({self.fileName})...")
         _LOGGER.debug(f"Recipe comment: {self.comment}")
 
         for source in self.sources:
 
             if source != None:
-                print(f"Running recipe for source file '{source}'")
+                print(f"📜📄 Running steps for source file '{source}'")
 
             if not self.__elaborateSteps(source):
-                print(f"❌ File '{source}' failed")
+                print(f"📜❌ File '{source}' failed")
                 return False
             
-            print(f"✅ File '{source}' passed")
+            print(f"📜✅ File '{source}' passed")
             
-        _LOGGER.info("✅ All steps succeeded.")
+        print(f"✅ Recipe '{self.name}' passed successfully.")
         return True
         
 
