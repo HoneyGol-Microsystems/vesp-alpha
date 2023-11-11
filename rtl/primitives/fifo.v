7fd0ca6c562161a012952b6b2c4a22bdfaf4d4fc
Ondrej Golasowski <golasowski.o@gmail.com>
2023-11-11 13:02:20 +0100

    Fixed fifo to use non-blocking assignments.

--- rtl/primitives/fifo.v
+++ rtl/primitives/fifo.v
@@ -7,77 +7,52 @@ module fifo #(
     input                 we,
     input                 re,
     input  [XLEN-1:0]     di,
-    output                empty,
-    output                full,
+    output reg            empty,
+    output reg            full,
     output [XLEN-1:0]     do
 );
 
 reg [$clog2(LENGTH) - 1:0] frontPointer, backPointer;
 reg [XLEN-1:0] memory [LENGTH-1:0];
-reg lastWrite, lastRead;
 
-always @( posedge clk ) begin
-    
-    if ( reset ) begin
-        frontPointer <= 0;
-        backPointer  <= 0;
-        empty        <= 1;
-        full         <= 0;
-    end else begin
-        
-        if ( we && !full ) begin
-            memory[backPointer] <= di;
-            backPointer <= backPointerInc;
-            empty <= 0;
-            if ( frontPointer == backPointerInc ) begin
-                full <= 1;
-            end
-        end else if ( re && !empty ) begin
-            frontPointer <= frontPointerInc;
-            full <= 0;
-            if ( frontPointerInc == backPointer ) begin
-                empty <= 1;
-            end
-        end
-    end
-end
-
-always @( * ) begin : fifo_status
-
-    if ( lastWrite && !lastRead ) begin
-        empty = 0;
-    end else if ( !lastWrite && lastRead ) begin
-        
-    end else begin
-        
-    end
-end
+wire [$clog2(LENGTH) - 1:0] frontPointerInc, backPointerInc;
 
 always @( posedge clk ) begin : fifo_operations
     
     if ( reset ) begin
+
         frontPointer <= 0;
         backPointer  <= 0;
-        lastWrite    <= 0;
-        lastRead     <= 0;
+        empty        <= 1;
+        full         <= 0;
+
     end else begin        
+
         if ( we && !full ) begin
             memory[backPointer] <= di;
-            backPointer <= backPointer + 1;
-            lastWrite   <= 1;
-        end else begin
-            lastWrite <= 0;
+            backPointer         <= backPointerInc;
+
+            if ( !re ) begin
+                empty <= 0;
+                full  <= ( frontPointer == backPointerInc ) ? 1'b1 : 1'b0;
+            end
         end
 
         if ( re && !empty ) begin
-            frontPointer <= frontPointer + 1;
-            lastRead     <= 1;
-        end else begin
-            lastRead <= 0;
+            frontPointer <= frontPointerInc;
+
+            if ( !we ) begin
+                empty <= ( frontPointerInc == backPointer ) ? 1'b1 : 1'b0;
+                full  <= 0;
+            end
         end
     end
 end
 
-assign do    = memory[frontPointer];
+assign do = memory[frontPointer];
+
+// Incrementation needs to be separated to keep correct width when comparing.
+assign frontPointerInc = frontPointer + 1;
+assign backPointerInc  = backPointer  + 1;
 
 endmodule
