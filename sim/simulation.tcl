f939aa5d974775d870c3e58b3257694350fadae9
Ondrej Golasowski <golasowski.o@gmail.com>
2023-11-12 18:32:04 +0100

    fixed tcl sim script

--- sim/simulation.tcl
+++ sim/simulation.tcl
@@ -11,22 +11,19 @@ set output_directory ./build/vivado
 file mkdir $output_directory
 
 # Setting up sources.
-read_verilog [ glob "./rtl/components/*.v" ]
-read_verilog [ glob "./rtl/primitives/*.v" ]
-read_verilog "./rtl/constants.vh"
+source ./sim/common/design_filelist.tcl
 read_verilog "./tests/hwtests/fifoTest.v"
 read_verilog "./tests/testConstants.vh"
 
 save_project_as -force sim_proj build/vivado
-set_property is_global_include true [ get_files constants.vh ]
 set_property is_global_include true [ get_files testConstants.vh ]
 set_property top fifoTest [get_fileset sim_1]
 set_property include_dirs "./" [get_fileset sim_1]
-# launch_simulation -simset sim_1 -mode behavioral
+launch_simulation -simset sim_1 -mode behavioral
+restart
 open_vcd sim.vcd
-log_vcd -level 0 [ get_objects /fifoTest/* ]
-log_vcd -level 0 [ get_objects /fifoTest/fifoInst/* ]
-# run
+log_vcd -level 0 [ get_scopes /fifoTest ]
+run
 flush_vcd
 close_vcd
 quit
@@ -34,5 +31,4 @@ quit
 
 # This command can be used to check whether is the constants.vh correctly set as global include.
 # report_property [ get_files constants.vh ]
-
-# Running simulation.
+# Or we can simple open the GUI.
