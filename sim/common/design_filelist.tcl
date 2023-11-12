eb1e16940e7608fbb38e7bd24c6fc06274ea3dc1
Ondrej Golasowski <golasowski.o@gmail.com>
2023-11-12 18:32:12 +0100

    added files to filelist

--- sim/common/design_filelist.tcl
+++ sim/common/design_filelist.tcl
@@ -2,3 +2,7 @@
 # Design file list
 # #################################
 # List of all files of the design. This is universally usable for both simulation and synthesis.
+read_verilog [ glob "./rtl/components/*.v" ]
+read_verilog [ glob "./rtl/primitives/*.v" ]
+read_verilog "./rtl/constants.vh"
+set_property is_global_include true [ get_files constants.vh ]
