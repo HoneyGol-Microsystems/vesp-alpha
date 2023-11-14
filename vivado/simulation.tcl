2034cd1b5a3f4078217009dca3b566e893d198be
Ondrej Golasowski <golasowski.o@gmail.com>
2023-11-14 16:55:55 +0100

    Changed scripts structure

--- vivado/simulation.tcl
+++ vivado/simulation.tcl
@@ -1,31 +1,26 @@
 # ########################## 
 # Vivado simulation script
 # ##########################
-# Using non-project mode.
+# Using project mode.
 #
 # Arguments
 # argv[0]: path to test top
 # argv[1]: test top name
 
 # Defining default value for parameters.
-set output_directory ./build/vivado
 set test_file [lindex $argv 0]
 set test_top_name [lindex $argv 1]
 
-# Preparing environment.
-file mkdir $output_directory
-
-# Setting up design sources.
-source ./sim/common/design_filelist.tcl
+# Creating a temp project.
+# Command line args have to be passed in this goofy way sadly.
+set argc 2
+set argv [list ./build/vivado sim_temp]
+source ./vivado/create_project.tcl
 
 # Setting up test-specific sources.
 read_verilog $test_file
-read_verilog "./tests/testConstants.vh"
-set_property is_global_include true [ get_files testConstants.vh ]
 
-save_project_as -force sim_proj build/vivado
 set_property top $test_top_name [get_fileset sim_1]
-set_property include_dirs "./" [get_fileset sim_1]
 launch_simulation -simset sim_1 -mode behavioral
 # Restart is needed to dump VCDs properly.
 restart
