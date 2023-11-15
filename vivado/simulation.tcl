613cd7d5881afc863c79777e611e7e6aee23df6f
Ondrej Golasowski <golasowski.o@gmail.com>
2023-11-15 17:40:01 +0100

    Added support for adding another file in sim, changed to run forever until finish.

--- vivado/simulation.tcl
+++ vivado/simulation.tcl
@@ -6,10 +6,12 @@
 # Arguments
 # argv[0]: path to test top
 # argv[1]: test top name
+# argv[2]: another file to add (e.g. hex executable)
 
 # Defining default value for parameters.
 set test_file [lindex $argv 0]
 set test_top_name [lindex $argv 1]
+set another_file [lindex $argv 2]
 
 # Creating a temp project.
 # Command line args have to be passed in this goofy way sadly.
@@ -20,6 +22,11 @@ source ./vivado/create_project.tcl
 # Setting up test-specific sources.
 read_verilog $test_file
 
+# Add another file if specified.
+if { $another_file ne "" } {
+    add_files -fileset sim_1 -norecurse $another_file
+}
+
 set_property top $test_top_name [get_fileset sim_1]
 launch_simulation -simset sim_1 -mode behavioral
 # Restart is needed to dump VCDs properly.
@@ -27,7 +34,7 @@ restart
 open_vcd sim.vcd
 # Log everything.
 log_vcd -level 0 [ get_scopes /* ]
-run
+run -all
 flush_vcd
 close_vcd
 quit
