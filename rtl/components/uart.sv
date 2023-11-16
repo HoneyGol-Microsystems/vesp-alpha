b58f3f52aee9830cd7d42e88fe8ac49d952fc19d
Ondrej Golasowski <golasowski.o@gmail.com>
2023-11-16 21:55:58 +0100

    Added clk dividers and queues

--- rtl/components/uart.sv
+++ rtl/components/uart.sv
@@ -1,4 +1,4 @@
-module moduleName #(
+module uart #(
     parameter DATA_WIDTH    = 32,
     parameter RX_QUEUE_SIZE = 16,
     parameter TX_QUEUE_SIZE = 16
@@ -7,6 +7,7 @@ module moduleName #(
     input  logic [2:0]             regsel;
     input  logic [DATA_WIDTH-1:0]  din;
     input  logic                   clk;
+    input  logic                   reset;
 
     output logic                   irq; 
     output logic [DATA_WIDTH-1:0]  dout;
@@ -14,10 +15,68 @@ module moduleName #(
     output logic                   rx;
 );
 
+    logic [4:0] main_clk_timer_val;
+    logic       main_clk;
+    logic [3:0] bit_clk_timer_val;
+    logic       bit_clk;
+
     logic [7:0] tx_data;
     logic [7:0] rx_data;
     logic [7:0] config_a;
     logic [7:0] status_a;
+
+    logic rx_we;
+    logic rx_re;
+    logic rx_di;
+    logic rx_empty;
+    logic rx_full;
+
+    logic tx_we;
+    logic tx_re;
+    logic tx_do;
+    logic tx_empty;
+    logic tx_full;
+
+    //////////////////////////////////////////////////
+    // QUEUES
+    //////////////////////////////////////////////////
+
+    assign rx_re = !we && ( regsel == 3'h1 );
+    assign status_a = { tx_full, rx_empty, 6'b0 };
+
+    assign tx_we = we && ( regsel == 3'h0 );
+
+    fifo #(
+        .XLEN(8),
+        .LENGTH(RX_QUEUE_SIZE)
+    ) rx_queue (
+        .clk(clk),
+        .reset(reset),
+        .we(rx_we),
+        .re(rx_re),
+        .di(rx_di),
+        .empty(rx_empty),
+        .full(rx_full),
+        .do(rx_data)
+    );
+
+    fifo #(
+        .XLEN(8),
+        .LENGTH(TX_QUEUE_SIZE)
+    ) tx_queue (
+        .clk(clk),
+        .reset(reset),
+        .we(tx_we),
+        .re(tx_re),
+        .di(tx_data),
+        .empty(tx_empty),
+        .full(tx_full),
+        .do(tx_do)
+    );
+
+    //////////////////////////////////////////////////
+    // REGISTER INTERFACE
+    //////////////////////////////////////////////////
 
     always_ff @( posedge clk ) begin : register_write
         if ( we ) begin
@@ -35,7 +94,6 @@ module moduleName #(
         end
     end
 
-
     always_comb begin : register_read
         case ( regsel )
             3'h0:    dout = { 24{ 1'b0 }, tx_data               };
@@ -44,5 +102,33 @@ module moduleName #(
             default: dout = { 16{ 1'b0 }, status_a,  8{ 1'b0  } };
         endcase
     end
+
+    //////////////////////////////////////////////////
+    // CLOCK DIVIDERS
+    //////////////////////////////////////////////////
+
+    always_ff @( posedge clk ) begin : main_clk_timer
+        if ( reset )
+            main_clk_timer <= 0;
+        else
+            main_clk_timer <= main_clk_timer + 1;
+    end
+
+    assign main_clk = main_clk_timer == 26;
+
+    always_ff @( posedge clk ) begin : bit_clk_timer
+        if ( reset )
+            bit_clk_timer_val <= 0;
+        else
+            bit_clk_timer_val <= bit_clk_timer_val + 1;
+    end
+
+    assign bit_clk = bit_clk_timer == 15;
+
+    //////////////////////////////////////////////////
+    // RX/TX
+    //////////////////////////////////////////////////
+
     
+
 endmodule
