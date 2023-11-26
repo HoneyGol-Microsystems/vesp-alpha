0ab77d48ccc06c76171e16773083a0be8b774bc6
Ondrej Golasowski <golasowski.o@gmail.com>
2023-11-26 19:07:46 +0100

    Added comment.

--- rtl/components/uart.sv
+++ rtl/components/uart.sv
@@ -81,6 +81,7 @@ module uart #(
     // QUEUES
     //////////////////////////////////////////////////
 
+    // nelze !!! provedlo by se u kazdeho shodneho regselu
     assign rx_re = !we && ( regsel == 3'h1 );
     
     assign status_a.tx_queue_full = tx_full;
