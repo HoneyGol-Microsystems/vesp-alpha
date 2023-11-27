a420b0c3edd5f3371fabfeaa7a908e0cd5d61b83
Jan Medek <medekja5@fit.cvut.cz>
2023-11-27 18:44:40 +0100

    update to master 2174dd7 (add GPIO)

--- software/example/program.c
+++ software/example/program.c
@@ -2,7 +2,23 @@
 
 int main ( void )
 {
-    GPIORD_B |= 0x1;
+    GPIODIR_A &= 0x00;
+    GPIODIR_B &= 0x00;
+
+    uint8_t a = GPIORD_A;
+    uint8_t b = GPIORD_B;
+
+    __asm ( "nop" );
+    __asm ( "nop" );
+
+    GPIODIR_A |= 0xFF;
+    GPIODIR_B |= 0xFF;
+
+    // write some data to pins
+    GPIOWR_A |= a + 1;
+    GPIOWR_B |= b + 1;
+
+    __asm ( ".word 0x1" );
 
     return 0;
 }
