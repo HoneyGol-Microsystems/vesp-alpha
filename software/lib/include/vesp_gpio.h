f483086790c16a96cdd45d6f9f7be93dfa4e5f33
Jan Medek <medekja5@fit.cvut.cz>
2023-11-28 16:25:09 +0100

    move assignment of GPIO vars to C src

--- software/lib/include/vesp_gpio.h
+++ software/lib/include/vesp_gpio.h
@@ -2,12 +2,12 @@
 #define __VESP_GPIO_H
 
 /* GPIO addresses */
-volatile uint8_t * const GPIOWR_A_PTR  = (volatile uint8_t *) 0xF0000000;
-volatile uint8_t * const GPIODIR_A_PTR = (volatile uint8_t *) 0xF0000001;
-volatile uint8_t * const GPIORD_A_PTR  = (volatile uint8_t *) 0xF0000002;
-volatile uint8_t * const GPIOWR_B_PTR  = (volatile uint8_t *) 0xF0000003;
-volatile uint8_t * const GPIODIR_B_PTR = (volatile uint8_t *) 0xF0000004;
-volatile uint8_t * const GPIORD_B_PTR  = (volatile uint8_t *) 0xF0000005;
+extern volatile uint8_t * const GPIOWR_A_PTR;
+extern volatile uint8_t * const GPIODIR_A_PTR;
+extern volatile uint8_t * const GPIORD_A_PTR;
+extern volatile uint8_t * const GPIOWR_B_PTR;
+extern volatile uint8_t * const GPIODIR_B_PTR;
+extern volatile uint8_t * const GPIORD_B_PTR;
 
 /* GPIO address placeholders */
 #define GPIOWR_A  *GPIOWR_A_PTR
