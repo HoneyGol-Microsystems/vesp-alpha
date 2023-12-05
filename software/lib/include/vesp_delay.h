22c18203c149cb0bed5917b1dc79e0c367c1a88e
Jan Medek <medekja5@fit.cvut.cz>
2023-12-05 16:14:53 +0100

    edit CPU_TICK macros to match 50 MHz frequency

--- software/lib/include/vesp_delay.h
+++ software/lib/include/vesp_delay.h
@@ -1,8 +1,9 @@
 #ifndef __VESP_DELAY_H
 #define __VESP_DELAY_H
 
-#define CPU_TICK_MS 100000
-#define CPU_TICK_US 100
+/* frequency is 50 MHz, period 20ns */
+#define CPU_TICK_MS 50000
+#define CPU_TICK_US 50
 
 void delay_ms ( uint32_t ms );
 void delay_us ( uint32_t us );
