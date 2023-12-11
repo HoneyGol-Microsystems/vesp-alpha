fb6474bd7301736f65b61fcebd888db3137c1ea2
Ondrej Golasowski <golasowski.o@gmail.com>
2023-12-11 13:53:33 +0100

    Added enable and edge detection to synchronizer.

--- rtl/primitives/synchronizer.v
+++ rtl/primitives/synchronizer.v
@@ -6,21 +6,34 @@
     parameter STAGES = 2
 ) (
     input            clk,
+    input            en,
     input  [LEN-1:0] dataIn,
-    output [LEN-1:0] dataOut
+    output [LEN-1:0] dataOut,
+    output [LEN-1:0] rise,
+    output [LEN-1:0] fall
 );
     
     reg [LEN-1:0] buffer [STAGES-1:0];
     integer i;
 
     always @(posedge clk) begin
-        
-        buffer[0] <= dataIn;
-        
-        for (i = 1; i < STAGES; i = i + 1) begin
-            buffer[i] <= buffer[i - 1];
+        if (en) begin
+            buffer[0] <= dataIn;
+
+            for (i = 1; i < STAGES; i = i + 1) begin
+                buffer[i] <= buffer[i - 1];
+            end
         end
     end
+    generate
+        if (STAGES >= 2) begin
+            assign rise = ~buffer[STAGES-2]  &  buffer[STAGES-1];
+            assign fall =  buffer[STAGES-2]  & ~buffer[STAGES-1];
+        end else begin
+            assign rise = 0;
+            assign fall = 0;
+        end
+    endgenerate
 
     assign dataOut = buffer[STAGES - 1];
 
