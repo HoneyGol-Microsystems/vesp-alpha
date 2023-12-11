1c65758c92cdbef4fcc3e0fe815a0fb6aaffd59c
Ondrej Golasowski <golasowski.o@gmail.com>
2023-12-11 17:26:43 +0100

    Fixed synchronizer bugs, changed data types.

--- rtl/primitives/synchronizer.sv
+++ rtl/primitives/synchronizer.sv
@@ -5,38 +5,39 @@
     parameter LEN    = 32,
     parameter STAGES = 2    // At least 2 required.
 ) (
-    input            clk,
-    input            en,
-    input  [LEN-1:0] dataIn,
-    output [LEN-1:0] dataOut,
-    output [LEN-1:0] rise,
-    output [LEN-1:0] fall
+    input  logic           clk,
+    input  logic           en,
+    input  logic [LEN-1:0] dataIn,
+    output logic [LEN-1:0] dataOut,
+    output logic [LEN-1:0] rise,
+    output logic [LEN-1:0] fall
 );
     
-    reg [LEN-1:0] buffer [STAGES-1:0];
-    integer i;
+    // There is additional one stage for rise/fall detection to work!
+    // See scheme for reference.
+    logic   [LEN-1:0] buffer [STAGES:0];
+    integer           i;
 
     always @(posedge clk) begin
         if (en) begin
             buffer[0] <= dataIn;
 
-            // There is additional one stage for rise/fall detection to work!
-            // See scheme for reference.
             for (i = 1; i < STAGES + 1; i = i + 1) begin
                 buffer[i] <= buffer[i - 1];
             end
         end
     end
+    
     generate
         if (STAGES >= 2) begin
-            assign rise =  buffer[STAGES - 2]  & ~buffer[STAGES - 1];
-            assign fall = ~buffer[STAGES - 2]  &  buffer[STAGES - 1];
+            assign rise =  buffer[STAGES - 1]  & ~buffer[STAGES];
+            assign fall = ~buffer[STAGES - 1]  &  buffer[STAGES];
         end else begin
             $fatal("There must be at least two stages! (STAGES parameter error.)");
         end
     endgenerate
 
-    assign dataOut = buffer[STAGES - 2];
+    assign dataOut = buffer[STAGES - 1];
 
 endmodule
 
