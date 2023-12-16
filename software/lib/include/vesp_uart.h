e4ab3850dbc515b339de47d3a753ef8c36a134c9
Jan Medek <medekja5@fit.cvut.cz>
2023-12-16 15:41:58 +0100

    add C support for UART MM registers

---
+++ software/lib/include/vesp_uart.h
@@ -0,0 +1,20 @@
+#ifndef __VESP_UART_H
+#define __VESP_UART_H
+
+/* GPIO addresses */
+extern volatile uint8_t * const UART_TX_DATA_PTR;
+extern volatile uint8_t * const UART_RX_DATA_PTR;
+extern volatile uint8_t * const UART_CONFIG_A_PTR;
+extern volatile uint8_t * const UART_CONFIG_B_PTR;
+extern volatile uint8_t * const UART_STATUS_A_PTR;
+extern volatile uint8_t * const UART_IF_REG_PTR;
+
+/* GPIO address placeholders */
+#define UART_TX_DATA  *UART_TX_DATA_PTR
+#define UART_RX_DATA  *UART_RX_DATA_PTR
+#define UART_CONFIG_A *UART_CONFIG_A_PTR
+#define UART_CONFIG_B *UART_CONFIG_B_PTR
+#define UART_STATUS_A *UART_STATUS_A_PTR
+#define UART_IF_REG   *UART_IF_REG_PTR
+
+#endif // __VESP_UART_H
---
+++ software/lib/src/vesp_uart.c
@@ -0,0 +1,8 @@
+#include "vesp.h"
+
+volatile uint8_t * const UART_TX_DATA_PTR  = (volatile uint8_t *) 0xF0000010;
+volatile uint8_t * const UART_RX_DATA_PTR  = (volatile uint8_t *) 0xF0000011;
+volatile uint8_t * const UART_CONFIG_A_PTR = (volatile uint8_t *) 0xF0000012;
+volatile uint8_t * const UART_CONFIG_B_PTR = (volatile uint8_t *) 0xF0000013;
+volatile uint8_t * const UART_STATUS_A_PTR = (volatile uint8_t *) 0xF0000014;
+volatile uint8_t * const UART_IF_REG_PTR   = (volatile uint8_t *) 0xF0000015;
