0f297c1cfc7213223ac0cf868ae2e9517942006a
Jan Medek <medekja5@fit.cvut.cz>
2023-12-17 18:10:30 +0100

    add comments to uart test program

--- software/example/uart-test.c
+++ software/example/uart-test.c
@@ -2,8 +2,11 @@
 
 int main ( void )
 {
+    // configuration
     UART_CONFIG_A = 0x14; // 0001 0100
-    UART_CONFIG_B = 0x60; // 0110 0000
+    UART_CONFIG_B = 0x60; // 1011 0000
+
+    // tx data
     UART_TX_DATA = 0xAB;
     UART_TX_DATA = 0xCD;
     UART_TX_DATA = 0xEF;
