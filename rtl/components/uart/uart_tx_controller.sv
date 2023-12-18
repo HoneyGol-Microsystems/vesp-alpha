3e43582329a06e7af8cf3d5a52481e3e5565bbae
Jan Medek <medekja5@fit.cvut.cz>
2023-12-18 11:45:52 +0100

    fix timing in tx controller

--- rtl/components/uart/uart_tx_controller.sv
+++ rtl/components/uart/uart_tx_controller.sv
@@ -1,9 +1,9 @@
 
 (* dont_touch = "yes" *) module uart_tx_controller (
     input  logic       clk,
-    input  logic       tx_clk_en,
     input  logic       reset,
     input  logic       parity_en,
+    input  logic       tx_send_bit,
     input  logic       tx_queue_empty,
     input  logic       double_stop_bit,
     input  logic       tx_bits_cnt_top,
@@ -23,6 +23,8 @@
     /////////////////////////////////////////////////////////////////////////
     enum {
         TX_IDLE,
+        TX_START_BIT,
+        TX_HOLD_DATA,
         TX_SEND_DATA,
         TX_PARITY_BIT,
         TX_STOP_BIT_2
@@ -35,9 +37,7 @@
         if (reset) begin
             tx_state <= TX_IDLE;
         end else begin
-            if (tx_clk_en) begin
-                tx_state <= tx_next_state;
-            end
+            tx_state <= tx_next_state;
         end
     end
 
@@ -47,39 +47,63 @@
     always_comb begin : tx_next_state_logic_proc
         case (tx_state)
             TX_IDLE: begin
-                if (!tx_queue_empty) begin
-                   tx_next_state = TX_SEND_DATA;
-                end else begin
-                   tx_next_state = TX_IDLE;
-                end
-            end
-
-            TX_SEND_DATA: begin
-                if (tx_bits_cnt_top) begin
-                    if (parity_en) begin
-                        tx_next_state = TX_PARITY_BIT;
-                    end else begin
-                        if (double_stop_bit) begin
-                            tx_next_state = TX_STOP_BIT_2;
-                        end else begin
-                            tx_next_state = TX_IDLE;
-                        end
-                    end
-                end else begin
-                    tx_next_state = TX_SEND_DATA;
-                end
-            end
-
-            TX_PARITY_BIT: begin
-                if (double_stop_bit) begin
-                    tx_next_state = TX_STOP_BIT_2;
+                if (tx_send_bit && !tx_queue_empty) begin
+                    tx_next_state = TX_START_BIT;
                 end else begin
                     tx_next_state = TX_IDLE;
                 end
             end
 
+            TX_START_BIT: begin
+                if (tx_send_bit) begin
+                    tx_next_state = TX_HOLD_DATA;
+                end else begin
+                    tx_next_state = TX_START_BIT;
+                end
+            end
+
+            TX_HOLD_DATA: begin
+                if (tx_send_bit) begin
+                    if (tx_bits_cnt_top) begin
+                        if (parity_en) begin
+                            tx_next_state = TX_PARITY_BIT;
+                        end else begin
+                            if (double_stop_bit) begin
+                                tx_next_state = TX_STOP_BIT_2;
+                            end else begin
+                                tx_next_state = TX_IDLE;
+                            end
+                        end
+                    end else begin
+                        tx_next_state = TX_SEND_DATA;
+                    end
+                end else begin
+                    tx_next_state = TX_HOLD_DATA;
+                end
+            end
+
+            TX_SEND_DATA: begin
+                tx_next_state = TX_HOLD_DATA;
+            end
+
+            TX_PARITY_BIT: begin
+                if (tx_send_bit) begin
+                    if (double_stop_bit) begin
+                        tx_next_state = TX_STOP_BIT_2;
+                    end else begin
+                        tx_next_state = TX_IDLE;
+                    end
+                end else begin
+                    tx_next_state = TX_PARITY_BIT;
+                end
+            end
+
             TX_STOP_BIT_2: begin
-                tx_next_state = TX_IDLE;
+                if (tx_send_bit) begin
+                    tx_next_state = TX_IDLE;
+                end else begin
+                    tx_next_state = TX_STOP_BIT_2;
+                end
             end
 
             default: tx_next_state = TX_IDLE;
@@ -101,9 +125,13 @@
 
         case (tx_state)
             TX_IDLE: begin
-                if (tx_queue_empty) begin
+                if (!tx_send_bit || tx_queue_empty) begin
                     tx_out_sel = 2'b01;
-                end else begin
+                end
+            end
+
+            TX_START_BIT: begin
+                if (tx_send_bit) begin
                     tx_queue_re       = 1;
                     tx_shift_reg_we   = 1;
                     tx_bits_cnt_reset = 1;
@@ -111,23 +139,35 @@
                 end
             end
 
-            TX_SEND_DATA: begin
-                if (tx_bits_cnt_top) begin
-                    if (parity_en) begin
-                        tx_out_sel = 2'b11;
+            TX_HOLD_DATA: begin
+                if (tx_send_bit) begin
+                    if (tx_bits_cnt_top) begin
+                        if (parity_en) begin
+                            tx_out_sel = 2'b11;
+                        end else begin
+                            tx_out_sel = 2'b01;
+                        end
                     end else begin
-                        tx_out_sel = 2'b01;
+                        tx_bits_cnt_en  = 1;
+                        tx_out_sel      = 2'b10;
+                        tx_shift_reg_se = 1;
+                        tx_parity_we    = 1;
                     end
                 end else begin
-                    tx_bits_cnt_en  = 1;
-                    tx_out_sel      = 2'b10;
-                    tx_shift_reg_se = 1;
-                    tx_parity_we    = 1;
+                    tx_out_sel = 2'b10;
                 end
             end
 
+            TX_SEND_DATA: begin
+                tx_out_sel = 2'b10;
+            end
+
             TX_PARITY_BIT: begin
-                tx_out_sel = 2'b01;
+                if (tx_send_bit) begin
+                    tx_out_sel = 2'b01;
+                end else begin
+                    tx_out_sel = 2'b11;
+                end
             end
 
             TX_STOP_BIT_2: begin
