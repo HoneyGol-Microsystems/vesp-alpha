998243d2bf049c2920fdce5019e4acb8210dcb8f
Jan Medek <medekja5@fit.cvut.cz>
2023-12-18 23:08:18 +0100

    edit program for bitstream testing

--- software/example/uart-test.c
+++ software/example/uart-test.c
@@ -4,14 +4,13 @@ int main ( void )
 {
     // configuration
     UART_CONFIG_A = 0xB4; // 1011 0100
-    UART_CONFIG_B = 0x60; // 1011 0000
+    UART_CONFIG_B = 0x30; // 0011 0000
 
-    // tx data
-    UART_TX_DATA = 0xAB;
-    UART_TX_DATA = 0xCD;
-    UART_TX_DATA = 0xEF;
-
-    while ( 1 );
+    while ( 1 )
+    {
+        UART_TX_DATA = 0x41;
+        delay_ms ( 1000 );
+    }
 
     return 0;
 }
